e9c89ce03449c077056375a59154dfd745f68b92
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-01-19 20:29:03 +0100

    add tools icon and change app.css to app.scss with bem

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 import logo from '../assets/logo.svg';
-import './App.css';
+import react from '../assets/logo-react.svg';
+import js from '../assets/logo-js.png';
+import html from '../assets/logo-html5.png';
+import css from '../assets/logo-css3.png';
+import sass from '../assets/logo-sass.svg';
+import eslint from '../assets/logo-eslint.svg';
+import prettier from '../assets/logo-prettier.png';
+import './App.scss';
 
 const App = () => (
-  <div classNme="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>My template for React-App with eslint, husky, prettier.</p>
+  <div classNme="app">
+    <header className="app__header">
+      <p className="app__paragraph">My template for React-App.</p>
+      <img src={logo} className="app__logo" alt="logo" />
+      <div className="icon">
+        <img src={react} className="icon__react" alt="logo React" title="React.js" />
+        <img src={js} className="icon__js" alt="logo JavaScript" title="JavaScript" />
+        <img src={html} className="icon__html" alt="logo Html" title="HTML5" />
+        <img src={css} className="icon__css" alt="logo Css" title="CSS3" />
+        <img src={sass} className="icon__sass" alt="logo Sass" title="Sass" />
+        <img src={eslint} className="icon__eslint" alt="logo ESlint" title="ESlint" />
+        <img src={prettier} className="icon__prettier" alt="logo Prettier" title="Prettier" />
+      </div>
     </header>
   </div>
 );
