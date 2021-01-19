43aba3009d0d977c7287090998f9f5723c8baf13
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-01-19 14:31:23 +0100

    initial commit, add eslint, prettier, husky with lint-staged

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div classNme="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>My template for React-App with eslint, husky, prettier.</p>
+    </header>
+  </div>
+);
+
+export default App;
